fe40b6a2ac0295314f391696f4806304100570ed
Pain <holtbrenden@yahoo.com>
2022-04-19 11:09:03 -0600

    starter nav/home

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NavBar/>
+    <div>
+      <Home/>
     </div>
+    </>
+  //  <Router>
+     
+  //    <Routes>
+     
+  //     <Route path='/' element='Hi'/>
+  //     <Route path='/what' element='Whats Up'/>
+
+  //    </Routes>
+  //  </Router>
+
+      
+  
   );
 }
 
