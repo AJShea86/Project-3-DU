aa963597e41d31c63e666df114e4c96fe290e4b2
samposig <samposig@gmail.com>
2022-04-19 11:01:54 -0600

    added usermodels and typedef

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/fetchDB", {
+mongoose.connect("mongodb://127.0.0.1:27017/fetchDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
