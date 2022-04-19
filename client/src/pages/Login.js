77ab2f160db9bf475f841b52390c0c29fc100bf9
Pain <holtbrenden@yahoo.com>
2022-04-19 13:59:58 -0600

    routes

---
+++ client/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>I am the Login Page</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1>I am The Register Page</h1>
+    </div>
+  )
+}
+
+export default Register
