cddcade603d1c94fb65d263735fbd6888e7ac262
samposig <samposig@gmail.com>
2022-04-21 11:12:47 -0600

    fixing

--- client/src/index.js
+++ client/src/index.js
@@ -19,10 +19,8 @@ import {
   const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ApolloProvider client={client}> */}
 
       <App />
-    {/* </ApolloProvider> */}
     
     
   </React.StrictMode>
