eb85e7cfa132016e9b88b397ca42e170c4c1ea76
samposig <samposig@gmail.com>
2022-04-21 13:41:38 -0600

    card

--- client/src/components/ImageSlider.js
+++ client/src/components/ImageSlider.js
@@ -3,7 +3,7 @@ import Card from "@mui/material/Card";
 // import { User } from "../../../server/models";
 import { useQuery } from "@apollo/client";
 import { GET_USERS } from "../utils/queries";
-
+//card
   const ImageSlider = () => {
     const { loading, data } = useQuery(GET_USERS);
     const users = data?.users || [];
