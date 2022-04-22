bd02336bcf14c9451c95314589bfab8f3188c515
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-22 11:22:22 -0600

    NavBar renders conditional data

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,37 +1,69 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PetsIcon from '@mui/icons-material/Pets';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PetsIcon from "@mui/icons-material/Pets";
+import Auth from "../utils/auth";
+import { useState } from "react";
 
 const NavBar = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static" style={{backgroundColor: 'black'}}>
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <PetsIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Fetch
-        </Typography>
-        <Button href='/' color='inherit'>Home</Button>
-        <Button href='/login' color="inherit">Login</Button>
-        <Button href='/register' color="inherit">Register</Button>
-      </Toolbar>
-    </AppBar>
-  </Box>
-);
-  
-}
+	const [loggedIn, setLoggedIn] = useState(Auth.loggedIn());
 
-export default NavBar
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static" style={{ backgroundColor: "black" }}>
+				{loggedIn ? (
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							sx={{ mr: 2 }}
+						>
+							<PetsIcon />
+						</IconButton>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Fetch
+						</Typography>
+						<Button href="/" color="inherit">
+							Home
+						</Button>
+						<Button color="inherit" onClick={Auth.logout}>
+							Logout
+						</Button>
+					</Toolbar>
+				) : (
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							sx={{ mr: 2 }}
+						>
+							<PetsIcon />
+						</IconButton>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Fetch
+						</Typography>
+						<Button href="/" color="inherit">
+							Home
+						</Button>
+						<Button href="/login" color="inherit">
+							Login
+						</Button>
+						<Button href="/register" color="inherit">
+							Register
+						</Button>
+					</Toolbar>
+				)}
+			</AppBar>
+		</Box>
+	);
+};
+
+export default NavBar;
