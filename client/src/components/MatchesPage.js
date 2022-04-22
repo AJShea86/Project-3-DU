19ed52312d3bd0a610c6766080663c5445330153
Andrew S <andrewsheamls@gmail.com>
2022-04-22 11:19:09 -0600

    made a matches page and button to link it

---
+++ client/src/components/MatchesPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from "@mui/material/Card";
+
+const MatchesPage = () => {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <h1>Matches</h1>
+        <Card body>This is some text within a card body.</Card>
+
+
+
+    </div>
+  )
+}
+
+export default MatchesPage
