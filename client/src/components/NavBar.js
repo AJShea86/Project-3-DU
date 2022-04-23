5b017686d2b34e627007e28507902fc8b03e4c07
Andrew S <andrewsheamls@gmail.com>
2022-04-23 12:04:52 -0600

    cleaning up

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,69 +8,74 @@ import IconButton from "@mui/material/IconButton";
 import PetsIcon from "@mui/icons-material/Pets";
 import Auth from "../utils/auth";
 import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 const NavBar = () => {
-	const [loggedIn, setLoggedIn] = useState(Auth.loggedIn());
+  const [loggedIn, setLoggedIn] = useState(Auth.loggedIn());
 
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static" style={{ backgroundColor: "black" }}>
-				{loggedIn ? (
-					<Toolbar>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
-							href="/"
-						>
-							<PetsIcon />
-						</IconButton>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							Fetch
-						</Typography>
-						<Button href="/main" color="inherit">
-							Find New Matches
-						</Button>
-						<Button href="/profile" color="inherit">
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ backgroundColor: "black" }}>
+        {loggedIn ? (
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              href="/"
+            >
+              <PetsIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Fetch
+            </Typography>
+            <Button href="/main" color="inherit">
+              Find New Matches
+            </Button>
+            {/* <IconButton href="/profile">
+              <SearchIcon fontSize="large" className="header__icon" />
+            </IconButton> */}
+
+            {/* <Button href="/profile" color="inherit">
 							Edit Profile
 						</Button>
 						<Button href="/matches" color="inherit">
 							View Matches
-						</Button>
-						<Button href="/" color="inherit" onClick={Auth.logout}>
-							Logout
-						</Button>
-					</Toolbar>
-				) : (
-					<Toolbar>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
-						>
-							<PetsIcon />
-						</IconButton>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							Fetch
-						</Typography>
-						<Button href="/" color="inherit">
-							Home
-						</Button>
-						<Button href="/login" color="inherit">
-							Login
-						</Button>
-						<Button href="/register" color="inherit">
-							Register
-						</Button>
-					</Toolbar>
-				)}
-			</AppBar>
-		</Box>
-	);
+						</Button> */}
+            <Button href="/" color="inherit" onClick={Auth.logout}>
+              Logout
+            </Button>
+          </Toolbar>
+        ) : (
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <PetsIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Fetch
+            </Typography>
+            <Button href="/" color="inherit">
+              Home
+            </Button>
+            <Button href="/login" color="inherit">
+              Login
+            </Button>
+            <Button href="/register" color="inherit">
+              Register
+            </Button>
+          </Toolbar>
+        )}
+      </AppBar>
+    </Box>
+  );
 };
 
 export default NavBar;
