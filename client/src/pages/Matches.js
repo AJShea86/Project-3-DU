084ecd318f94f8e7cc9d6c3b206efbbc5b30c365
Andrew S <andrewsheamls@gmail.com>
2022-04-23 14:10:04 -0600

    fixed nav bar and adjusted card height

--- client/src/pages/Matches.js
+++ client/src/pages/Matches.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import MatchesPage from '../components/MatchesPage'
-import ProfileButton from '../components/ProfileButton'
 
 const Matches = () => {
   return (
     <div>
-        <ProfileButton />
         <MatchesPage />
     </div>
   )
